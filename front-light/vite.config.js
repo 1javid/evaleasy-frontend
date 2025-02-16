ca8f2c17695557cfb4aab2b08e4f55e149384d78
Javid <jav.alakbarli@gmail.com>
2025-02-16 22:09:15 +0400

    fix routing for vercel dev

--- front-light/vite.config.js
+++ front-light/vite.config.js
@@ -3,4 +3,8 @@ import preact from "@preact/preset-vite";
 
 export default defineConfig({
   plugins: [preact()],
+  base: "/",  // Ensure correct routing on Vercel
+  build: {
+    outDir: "dist",
+  }
 });
